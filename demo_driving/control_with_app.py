a6ba21268619904addf50be85c049003685bf1c4
남효식 <namnoreng@gmail.com>
2025-07-28 13:01:11 +0900

    직진 보정 없앤 곳에서는 플러시 ㄴㄴ

--- demo_driving/control_with_app.py
+++ demo_driving/control_with_app.py
@@ -430,7 +430,6 @@ try:
                 print("[Client] 마커 2번 인식 완료, 추가 직진하여 두 번째 0번 마커로...")
                 
                 # 이제 0번 마커 인식 (두 번째 0번이어야 함)
-                driving.flush_camera(cap_front, 5)
                 print("[Client] 두 번째 0번 마커 인식 시작...")
                 driving.driving(cap_front, marker_dict, param_markers, marker_index=0, camera_matrix=camera_front_matrix, dist_coeffs=dist_front_coeffs, target_distance=0.36)
                 
